86843059a9012bb0c40e42a3bba6c774b60f1581
Neha Kanabar <nehakanabar99@gmail.com>
2024-09-21 13:39:50 +0530

    n

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 // backend/index.js
 const express = require('express');
 const cors = require("cors");
+
 const rootRouter = require("./routes/index");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+
+    res.json({
+        message: "Welcome to the DB!",
+   });
+})
+
 
 app.use("/api/v1", rootRouter);
 
