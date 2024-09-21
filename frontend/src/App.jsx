9c23a9b03ec2c9faca4de0edd2842886fb54e420
Neha Kanabar <nehakanabar99@gmail.com>
2024-09-21 12:11:36 +0530

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App(){
     <>
       <BrowserRouter>
       <Routes>
+          <Route path="/" element={<Signup />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard/>}/>
